5fffc4108d0ff5493edf1bdded895d12f191d7c6
wuyenlin <finne0831@gmail.com>
2020-06-15 08:37:06 +0800

    fixed PID,LQR comparison

--- NIKHIL_DATA/result.m
+++ NIKHIL_DATA/result.m
@@ -99,39 +99,39 @@ for i = 1:size(files,2)
 end
 
 %% mu-comparison
-% W_Controller_1mu_Straight
-FL_W = data{4}.data(:,1)*0.32;
-FR_W = data{4}.data(:,2)*0.32;
-RL_W = data{4}.data(:,3)*0.32;
-RR_W = data{4}.data(:,4)*0.32;
 % PID_1mu
-FL_PID = data{7}.data(:,1)*0.32;
-FR_PID = data{7}.data(:,2)*0.32;
-RL_PID = data{7}.data(:,3)*0.32;
-RR_PID = data{7}.data(:,4)*0.32;
+FL_PID = data{4}.data(:,1)*0.32;
+FR_PID = data{4}.data(:,2)*0.32;
+RL_PID = data{4}.data(:,3)*0.32;
+RR_PID = data{4}.data(:,4)*0.32;
+% W_Controller_1mu_Straight
+FL_W = data{5}.data(:,1)*0.32;
+FR_W = data{5}.data(:,2)*0.32;
+RL_W = data{5}.data(:,3)*0.32;
+RR_W = data{5}.data(:,4)*0.32;
 
 figure(37);
 
 t  = data{4}.data(:,6);
-plot(t, FL_W, '-','Color', [.1 .4 .9]);
+plot(t, FL_PID, '-','Color', [.1 .4 .9]);
 hold on;
-plot(t, FR_W, '--','Color', [.1 .4 .9]);
-plot(t, RL_W, '-.','Color', [.1 .4 .9]);
-plot(t, RR_W, ':','Color', [.1 .4 .9]);
-t  = data{7}.data(:,6);
-plot(t, FL_PID, '-','Color', [.9 .3 .2]);
-plot(t, FR_PID, '--','Color', [.9 .3 .2]);
-plot(t, RL_PID, '-.','Color', [.9 .3 .2]);
-plot(t, RR_PID, ':','Color', [.9 .3 .2]);
+plot(t, FR_PID, '--','Color', [.1 .4 .9]);
+plot(t, RL_PID, '-.','Color', [.1 .4 .9]);
+plot(t, RR_PID, ':','Color', [.1 .4 .9]);
+t  = data{5}.data(:,6);
+plot(t, FL_W, '-','Color', [.9 .3 .2]);
+plot(t, FR_W, '--','Color', [.9 .3 .2]);
+plot(t, RL_W, '-.','Color', [.9 .3 .2]);
+plot(t, RR_W, ':','Color', [.9 .3 .2]);
 xlabel('Time [s]');
 ylabel('Wheel speed [m/s]');
-legend('FL (LQR)','FR (LQR)','RL (LQR)','RR (LQR)', 'FL (PID)','FR (PID)','RL (PID)','RR (PID)');
+legend('FL (PID)','FR (PID)','RL (PID)','RR (PID)', 'FL (LQR)','FR (LQR)','RL (LQR)','RR (LQR)');
 hold off;
 grid on;
-set(gca, 'YColor', 'k');
 axis([10 18 -5 40]);
+set(gca,'LineWidth',2,'FontSize',15, 'YColor', 'k','XMinorTick','on');
 set(findall(gca, 'Type', 'Line'),'LineWidth',2);
-title('LQR, PID Controller Wheel Speed comparison on high friction road');
+title({'LQR, PID Controller Wheel Speed ';'comparison on high friction road'});
 axis([10 18 -5 40]);
 ax = gca;
 ax.XTick = 10:2:18;
